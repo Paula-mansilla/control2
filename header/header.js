35c614d534b4a96e1ca7aa95df9794993b2927b7
Paula Mansilla <pimansilla@scl.edu.gt>
2025-09-16 09:57:39 -0600

    sistema de carpetas

---
+++ header/header.js
@@ -0,0 +1,11 @@
+export function header() {
+    let nav = document.createElement("header");
+    nav.className = "header";
+
+    // texto
+    let title = document.createElement("h1");
+    title.textContent = "Lista de Compras";
+
+  
+  
+}
